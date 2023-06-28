263f47b8c59bebe03f9f4c33bda779f396b3b110
Yuya Satake <sata.yu.7878@gmail.com>
2023-06-28 14:19:32 -0700

    completed merge sort

--- sort/merge-sort.js
+++ sort/merge-sort.js
@@ -36,4 +36,30 @@ function merge(arr1, arr2) {
   return results;
 }
 
-console.log(merge([1, 10, 50], [2, 14, 99, 100]));
+// console.log(merge([1, 10, 50], [2, 14, 99, 100]));
+
+/**
+ * 1. break up the array into halves until you have arrays that are empty or have one element
+ * 2. once you have smaller sorted arrays, merge those arrays with other sorted arrays
+ *    until we are back at the full length of the array
+ * 3. once the array has been merged back together, return merged (and sorted) array
+ */
+
+function mergeSort(arr) {
+  // if (arr.length <= 1) return arr;
+  // const middle = Math.round(arr.length / 2);
+  // const left = arr.slice(0, middle);
+  // const right = arr.slice(middle);
+
+  // return merge(mergeSort(left), mergeSort(right));
+
+  // I think bellow is more readable
+  if (arr.length <= 1) return arr;
+  const middle = Math.round(arr.length / 2);
+  const left = mergeSort(arr.slice(0, middle));
+  const right = mergeSort(arr.slice(middle));
+
+  return merge(left, right);
+}
+
+console.log(mergeSort([3, 5, 7, 4, 9, 1]));
